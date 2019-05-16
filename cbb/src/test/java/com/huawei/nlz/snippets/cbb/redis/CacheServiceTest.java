75ecd8e15d1493714d74973d0748efef97b117c3
niluzhang <1585288467@qq.com>
2019-05-16 22:24:35 +0800

    增加redis缓存封装。

--- cbb/src/test/java/com/huawei/nlz/snippets/cbb/redis/CacheServiceTest.java
+++ cbb/src/test/java/com/huawei/nlz/snippets/cbb/redis/CacheServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class CacheServiceTest extends SpringJUnitContext {
@@ -30,9 +31,21 @@ public class CacheServiceTest extends SpringJUnitContext {
         log.info("niluzhang's session is {}",
                 JSON.toJSONString(cacheService.queryEntity("SESSION:123456789", Session.class)));
 
-        cacheService.deleteKey("mykey");
-        log.info("setIfAbsent mykey: {}", cacheService.setIfAbsent("mykey", "val"));  //true
-        log.info("setIfAbsent mykey: {}", cacheService.setIfAbsent("mykey", "val"));  //false
+        // redis key名和value都可以包含空格
+        cacheService.deleteKey("wechat 123456");
+        log.info("setIfAbsent mykey: {}", cacheService.setIfAbsent("wechat 123456", "val"));  //true
+        log.info("setIfAbsent mykey: {}", cacheService.setIfAbsent("wechat 123456", "val"));  //false
+
+        // key已存在，但是没有设置失效时间，getExpire返回-1
+        log.info(cacheService.getRedisTemplate().getExpire("wechat 123456") + "");
+        // key不存在，getExpire返回-2
+        log.info(cacheService.getRedisTemplate().getExpire("token") + "");
+        cacheService.setValue("token", "token");
+        cacheService.setKeyExpire("token", 10, TimeUnit.SECONDS);
+        // 返回10，可见getExpire默认以秒为单位
+        log.info(cacheService.getRedisTemplate().getExpire("token") + "");
+        // 剩余存活时间的毫秒数
+        log.info(cacheService.getRedisTemplate().getExpire("token", TimeUnit.MILLISECONDS) + "");
     }
 
     @Data
