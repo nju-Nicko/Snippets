0f7f4a31d3ddd5430268ed19d63f7adcd3d0dc6e
nju-Nicko <1585288467@qq.com>
2019-06-23 01:16:05 +0800

    添加长轮询服务端实现。

---
+++ cbb/src/main/java/com/huawei/nlz/snippets/cbb/longpolling/ChatFacade.java
@@ -0,0 +1,43 @@
+package com.huawei.nlz.snippets.cbb.longpolling;
+
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 长轮询服务端的一种实现
+ */
+public class ChatFacade {
+
+    /**
+     * 长轮询超时时间25秒
+     */
+    private static final int LONG_POLLING_TIMEOUT = 25000;
+
+    @SuppressWarnings("ConstantConditions")
+    public List<String> getMessage(String uid) {
+        long requestTime = System.currentTimeMillis();
+
+        List<String> resultMsgs = new ArrayList<>();
+
+        while (System.currentTimeMillis() - requestTime <= LONG_POLLING_TIMEOUT) {
+            List<String> msgs = null;  // 调用其他服务获取消息，这里省略掉了。
+
+            if (!CollectionUtils.isEmpty(msgs)) {
+                resultMsgs.addAll(msgs);
+                break;
+            }
+
+            try {
+                TimeUnit.SECONDS.sleep(1);  // 休眠1秒后继续轮询
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return resultMsgs;
+    }
+
+}
