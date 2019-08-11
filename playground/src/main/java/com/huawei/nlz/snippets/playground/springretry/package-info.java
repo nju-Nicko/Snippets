9231a29b1cb10c3d9525a5253dcb6b39cde73dd4
nju-Nicko <1585288467@qq.com>
2019-08-11 10:59:49 +0800

    增加spring-retry应用实例

--- playground/src/main/java/com/huawei/nlz/snippets/playground/springretry/package-info.java
+++ playground/src/main/java/com/huawei/nlz/snippets/playground/springretry/package-info.java
@@ -1,5 +1,8 @@
 /**
  * 基于Spring Retry书写优雅的重试代码。
+ *
+ * 一些资料参考：https://yq.aliyun.com/articles/92899
+ *             https://blog.csdn.net/u012731081/article/details/78892897
  */
 
 package com.huawei.nlz.snippets.playground.springretry;
