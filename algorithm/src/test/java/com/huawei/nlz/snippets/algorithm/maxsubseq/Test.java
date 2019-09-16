bb6a32f75bab5650574871bcd7c03418d3f5372b
nju-Nicko <1585288467@qq.com>
2019-09-16 22:52:40 +0800

    add max sub seq.

--- algorithm/src/test/java/com/huawei/nlz/snippets/algorithm/maxsubseq/Test.java
+++ algorithm/src/test/java/com/huawei/nlz/snippets/algorithm/maxsubseq/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public void testMaxSubSeq() {
         int[] arr = {4, -3, 5, -2, -1, 2, 6, -2};
         Assert.assertEquals(11, MaxSubSeqSolution.maxSubSeq(arr));
-        
+
         int[] arr2 = {-3, -2};
         Assert.assertEquals(0, MaxSubSeqSolution.maxSubSeq(arr2));
     }
