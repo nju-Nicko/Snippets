848ab662a7f52965fb2978fa0ea3ee101beda947
nju-Nicko <1585288467@qq.com>
2019-10-14 23:15:42 +0800

    添加Unicode增补字符的解析。

--- playground/src/main/java/com/huawei/nlz/snippets/playground/chars/Chars.java
+++ playground/src/main/java/com/huawei/nlz/snippets/playground/chars/Chars.java
@@ -20,11 +20,16 @@ public class Chars {
     public static void chars(String str) {
         Objects.requireNonNull(str);
         for (int i = 0; i <= str.length() - 1; ) {
+            // 获取代码点
             int codePoint = str.codePointAt(i);
+            // 判断代码点是不是位于Unicode增补平面
             if (Character.isSupplementaryCodePoint(codePoint)) {
+                // 如果位于增补平面，则该字符在UTF16下使用两个代码单元编码，
+                // 需要通过Character.toChars(int codePoint)输出
                 System.out.println(Character.toChars(codePoint));
                 i += 2;
             } else {
+                // 否则直接把int强转成char输出即可，只占一个代码单元
                 System.out.println((char) codePoint);
                 i += 1;
             }
